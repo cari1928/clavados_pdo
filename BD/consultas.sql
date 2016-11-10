13ad41291ac012562a449c14125760c995da86c9
Radogan <radogan1928@hotmail.com>
2016-11-10 17:18:49 -0600

    Ajax Juez Publico 2

--- BD/consultas.sql
+++ BD/consultas.sql
@@ -1,3 +1,7 @@
 select bandera from nacionalidad where cve_nacionalidad='AF
 '
 $prom/= 5;
+
+select * from ronda inner join enviarDatosJuez on enviarDatosJuez.cve_clavadista = ronda.cve_clavadista inner join clavado on clavado.cve_clavado = enviarDatosJuez.cve_clavado inner join clavadista on clavadista.cve_clavadista = ronda.cve_clavadista inner join nacionalidad on clavadista.cve_nacionalidad = nacionalidad.cve_nacionalidad order by cve_clavadista, num_ronda
+
+select * from ronda inner join enviarDatosJuez on enviarDatosJuez.cve_clavadista = ronda.cve_clavadista inner join clavado on clavado.cve_clavado = enviarDatosJuez.cve_clavado inner join clavadista on clavadista.cve_clavadista = ronda.cve_clavadista inner join nacionalidad on clavadista.cve_nacionalidad = nacionalidad.cve_nacionalidad order by clavadista.cve_clavadista, num_ronda
---
+++ conversationA.php
@@ -0,0 +1,62 @@
+<?php
+  //COMUNICACIÓN JUEZ - PUBLICO
+  include ('cl_web.class.php');
+
+  $web->conexion();
+
+  //ADMIN-JUEZ
+  $sql = "select distinct num_ronda, bandera, clavadista.cve_nacionalidad, nombre_completo, calif_ronda from ronda
+    inner join enviarDatosJuez on enviarDatosJuez.cve_clavadista = ronda.cve_clavadista
+    inner join clavado on clavado.cve_clavado = enviarDatosJuez.cve_clavado
+    inner join clavadista on clavadista.cve_clavadista = ronda.cve_clavadista
+    inner join nacionalidad on clavadista.cve_nacionalidad = nacionalidad.cve_nacionalidad
+  order by clavadista.cve_clavadista, num_ronda";
+  $result = $web->fetchAll($sql);
+
+  // echo $sql;
+  // echo "<pre>";
+  // var_dump($result);
+  $tmpJ = 0;
+
+  echo '<table id="interTable" class="table table-hover"> <!-- Lo cambiaremos por CSS -->
+     <tr class="success">
+        <td bgcolor="grey">CLASIFICACIÓN</td>
+        <td bgcolor="grey">BANDERA</td><!--BANDERA-->
+        <td bgcolor="grey">PAIS</td><!--PAIS-->
+        <td bgcolor="grey">NOMBRE</td><!--NOMBRE-->
+        <td bgcolor="grey">1</td>
+        <td bgcolor="grey">2</td>
+        <td bgcolor="grey">3</td>
+        <td bgcolor="grey">4</td>
+        <td bgcolor="grey">5</td>
+        <td bgcolor="grey">6</td>
+        <td bgcolor="grey">-</td><!---->
+        <td bgcolor="grey"7>+</td><!--"+"-->
+    </tr>';
+
+  for ($i=0; $i < count($result); $i++) {
+      $tmp = $result[$i]['nombre_completo'];
+      echo "<tr>";
+      echo "<td>".($i + 1)."</td>";
+      echo "<td>".$result[$i]['bandera']."</td>";
+      echo "<td>".$result[$i]['cve_nacionalidad']."</td>";
+      echo "<td>".$result[$i]['nombre_completo']."</td>";
+
+      // die();
+      for ($j=$i; $j < ($i+6); $j++) {
+        if(isset($result[$j])) {
+          if($tmp != $result[$j]['nombre_completo']) {
+            $j = $i + 6;
+          } else {
+            $tmpJ = $j;
+            echo "<td>".$result[$j]['calif_ronda']."</td>";
+          }
+        } else {
+          echo "<td></td>";
+        }
+      }
+      $i = $tmpJ + 1;
+
+    echo "</tr>";
+  }
+?>
